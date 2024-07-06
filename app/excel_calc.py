1f75e1169405621de79e1c2572617af344c7426e
nnmb007 <64924200+nnmb007@users.noreply.github.com>
2024-07-06 01:29:37 +0000

    Bug fixes

--- app/excel_calc.py
+++ app/excel_calc.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 st.header("Efficiency calculator for optimization")
-st.subheader("NOTE: This will not work unless it's a CSV file")
+
 
 # uploaded_file = st.file_uploader("Upload your CSV, XLS, or XLXS file", type=["xlxs", "xls"], key="uploaded_file")
 uploaded_file = st.file_uploader(
