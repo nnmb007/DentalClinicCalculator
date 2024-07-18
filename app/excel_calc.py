e1014c3f9e12f750b3d3a388c69b7e45bc320230
nnmb007 <nnmbotelho@gmail.com>
2024-07-18 13:15:25 -0700

    Added opticlinic.py which is a combination of both calculators into one place

--- app/excel_calc.py
+++ app/excel_calc.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.header("Efficiency calculator for optimization")
 st.subheader("NOTE: This will not work unless it's a CSV file")
 
-# uploaded_file = st.file_uploader("Upload your CSV, XLS, or XLXS file", type=["xlxs", "xls"], key="uploaded_file")
+
 uploaded_file = st.file_uploader(
     "Upload your CSV, XLS, or XLXS file", type=["csv", "xlsx"], accept_multiple_files=False, key="uploaded_file"
 )
